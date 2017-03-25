b7bbc37844b4e899de490d907ef990b335bf1553
jermiedomingo <8jermiedomingo@gmail.com>
2017-03-25 21:12:08 +0400

    typo fix & wording update for proper chatbot flow

--- playbot-webhooks/randomNumber.js
+++ playbot-webhooks/randomNumber.js
@@ -4,14 +4,14 @@ const getRandom = (min, max) => {
 
 	min = Math.ceil(min);
 	max = Math.floor(max);
-	retrn Math.floor(Math.random() * (max - min + 1)) + min;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 //arguments: context, callback
 module.exports = function(ctx, cb){
 
 	let getResult = getRandom(1,4);
 	return ctx.data.replyData == getResult ? cb(null, {
-		result: 'You got it right!',
+		result: 'You won!',
 		resultVal: String(getResult)
 	}) : cb(null, {
 		result: 'Nopee, you lost!',
