4bfec06bdb30e5a3a1273b2c2697d17260820f91
jermiedomingo <8jermiedomingo@gmail.com>
2017-06-25 13:40:17 +0400

    bot in action

--- rule-based-bot/app.js
+++ rule-based-bot/app.js
@@ -7,12 +7,26 @@ const rl = Readline.createInterface({
   terminal: false
 });
 
+const matcher = require('./matcher');
+
 rl.setPrompt('> ');
 rl.prompt();
 
 //event listener
 //every time a user inputs something
 rl.on('line', reply => {
-  console.log(`You said :: ${reply}`);
-  rl.prompt();
+
+  matcher(reply, data => {
+    switch (data.intent) {
+      case 'Hello':
+        console.log('Hello too');
+        rl.prompt();
+        break;
+      default: {
+        console.log('I don\'t know what you mean');
+        rl.prompt();
+      }
+
+    }
+  });
 });
